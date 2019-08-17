b19f456782ba0e85ddb71b49c75836d5f16fb3e3
Gilbert Rodríguez <gilbertrdz.dev@gmail.com>
2019-08-17 11:47:45 -0500

    Ajuste de espacios y líneas

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
-define(function(require,exports,module) {
-    
-    'use strict';
-	
-    var CommandManager      = brackets.getModule("command/CommandManager"),
-        Menus               = brackets.getModule("command/Menus"),
-        KeyBindingManager   = brackets.getModule("command/KeyBindingManager"),
-        PanelManager        = brackets.getModule("view/WorkspaceManager"),
-        ExtensionUtils      = brackets.getModule("utils/ExtensionUtils"),        
-        AppInit             = brackets.getModule("utils/AppInit");
+define( function ( require, exports, module ) {
 
-    var LAYERS_EXECUTE = "layers.execute";
-    var $panel;
-    var panelHtml = require("text!panel.html");
-	var $layersIcon = $('<a href="#" title="PSD Layer Viewer" id="brackets-psd-layers-icon"></a>');
+	'use strict';
 
-    // Keyboard shortcut
-	var registro = CommandManager.register("Open PSD Layer Viewer", LAYERS_EXECUTE, handleLayers);
-	KeyBindingManager.addBinding(LAYERS_EXECUTE, "Ctrl-Shift-Alt-P");
-    
-    function handleLayers() {
-        
-        if($panel.isVisible()) {
-            $panel.hide();
-			$layersIcon.removeClass('active');
-            CommandManager.get(LAYERS_EXECUTE).setChecked(false);
-        } else {
-            $panel.show();
-			$layersIcon.addClass('active');
-            CommandManager.get(LAYERS_EXECUTE).setChecked(true);
-        }
-    }
+	var CommandManager    = brackets.getModule( "command/CommandManager" ),
+		Menus             = brackets.getModule( "command/Menus" ),
+		KeyBindingManager = brackets.getModule( "command/KeyBindingManager" ),
+		PanelManager      = brackets.getModule( "view/WorkspaceManager" ),
+		ExtensionUtils    = brackets.getModule( "utils/ExtensionUtils" ),
+		AppInit           = brackets.getModule( "utils/AppInit" );
 
-    AppInit.appReady(function () {
-        $layersIcon.click(function () {
-            handleLayers();
-        }).appendTo("#main-toolbar .buttons");
-		
-		ExtensionUtils.loadStyleSheet(module, "styles.css");
-        $panel = PanelManager.createBottomPanel(LAYERS_EXECUTE, $(panelHtml),200);
+	var LAYERS_EXECUTE = "layers.execute";
+	var $panel;
+	var panelHtml = require( "text!panel.html" );
+	var $layersIcon = $( '<a href="#" title="PSD Layer Viewer" id="brackets-psd-layers-icon"></a>' );
 
-    });
-	
-});
+	// Keyboard shortcut
+	var registro = CommandManager.register( "Open PSD Layer Viewer", LAYERS_EXECUTE, handleLayers );
+	KeyBindingManager.addBinding( LAYERS_EXECUTE, "Ctrl-Shift-Alt-P" );
+
+	function handleLayers() {
+
+		if ( $panel.isVisible() ) {
+			$panel.hide();
+			$layersIcon.removeClass( 'active' );
+			CommandManager.get( LAYERS_EXECUTE ).setChecked( false );
+		} else {
+			$panel.show();
+			$layersIcon.addClass( 'active' );
+			CommandManager.get( LAYERS_EXECUTE ).setChecked( true );
+		}
+	}
+
+	AppInit.appReady( function () {
+		$layersIcon.click( function () {
+			handleLayers();
+		} ).appendTo( "#main-toolbar .buttons" );
+
+		ExtensionUtils.loadStyleSheet( module, "styles.css" );
+		$panel = PanelManager.createBottomPanel( LAYERS_EXECUTE, $( panelHtml ), 200 );
+
+	} );
+
+} );
